11fed67d7d80ca0c17ed519bf5a4ec34c778ba3e
Jason Hartford <jasonhar@cs.ubc.ca>
2018-01-14 18:52:07 -0800

    topic model training. TODO - minibatching

--- topic_model.py
+++ topic_model.py
@@ -53,21 +53,17 @@ def sample_dense_values_uniform_val(mask_indices, mask_tr_val_split, minibatch_s
 
 
 def ce_loss(log_prob, counts):
-    return - tf.reduce_mean(counts * log_prob)
+    return - tf.reduce_mean(tf.squeeze(counts) * tf.squeeze(log_prob))
 
-def masked_inner_product(nvec, mvec, mask, log_inp=False):
-    ng = tf.gather(nvec, mask[:,0], axis=0)
-    mg = tf.gather(mvec, mask[:,1], axis=1)
-    if log_inp:
-        # assumes that ng and mg are in log domain so we add and take exp instead of multiply to get inner product
-        return tf.reduce_sum(tf.exp(ng + tf.transpose(mg, (1, 0, 2))), axis=2, keep_dims=False)
-    else:
-        return tf.reduce_sum(ng * tf.transpose(mg, (1, 0, 2)), axis=2, keep_dims=False)
+def masked_inner_product(nvec, mvec, mask):
+    ng = tf.squeeze(tf.gather(nvec, mask[:,0], axis=0))
+    mg = tf.squeeze(tf.gather(mvec, mask[:,1], axis=1))
+    return tf.reduce_logsumexp(ng + mg, axis=1, keep_dims=False)
 
 def main(opts, data=None):        
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)    
     if data is None:
-        data = read_data()
+        data, top_words = read_data()
     
     N, M, num_features = data['mat_shape']
 
@@ -84,19 +80,23 @@ def main(opts, data=None):
 
                 model = Model(layers=opts['architecture'], layer_defaults=opts['defaults'], verbose=2) #define the model
                 model_output = model.get_output(tr_dict) #build the model
-                words = tf.nn.log_softmax(model_output['nvec'])
-                docs = tf.nn.log_softmax(model_output['mvec'], dim=0)
+                #words = tf.squeeze(tf.nn.log_softmax(model_output['nvec'], dim=0))
+                words = tf.nn.log_softmax(model_output['nvec'], dim=0)
+                docs = tf.nn.log_softmax(model_output['mvec'], dim=2)
+                #docs = tf.squeeze(tf.nn.log_softmax(model_output['mvec'], dim=-1))
 
             # log_prob_topics = words + docs # gather
             # take sum
-            total_prob = tf.clip_by_value(masked_inner_product(words, docs, mask_indices_tr, log_inp=True), 0., 1.)
+            eps = 1e-16
+            total_prob = tf.clip_by_value(masked_inner_product(words, docs, mask_indices_tr), -np.inf, 0.)
+            #total_prob = masked_inner_product(words, docs, mask_indices_tr, log_inp=True)
             topic_loss = ce_loss(total_prob, mat_values_tr)
             #loss and training
             reg_loss = sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)) # regularization
             total_loss = topic_loss + reg_loss
 
-            # train_step = tf.train.AdamOptimizer(opts['lr']).minimize(total_loss)
-            train_step = tf.train.RMSPropOptimizer(opts['lr']).minimize(total_loss)
+            train_step = tf.train.AdamOptimizer(opts['lr']).minimize(total_loss)
+            #train_step = tf.train.RMSPropOptimizer(opts['lr']).minimize(total_loss)
             sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
             sess.run(tf.global_variables_initializer())
 
@@ -122,7 +122,7 @@ def main(opts, data=None):
                     mat_values = data['mat_values_tr'][sample_]
                     mask_indices = data['mask_indices_tr'][sample_]
 
-                    tr_dict = {mat_values_tr:mat_values,
+                    tr_dict = {mat_values_tr:mat_values,# * 100,
                                 mask_indices_tr:mask_indices}
                     
                     _, bloss_, btopic_loss_ = sess.run([train_step, total_loss, topic_loss], feed_dict=tr_dict)
@@ -136,7 +136,12 @@ def main(opts, data=None):
                 losses['train'].append(loss_tr_)
                 losses['valid'].append(loss_val_)
 
-                print("epoch {:d} took {:.1f} training loss {:.3f} (rec:{:.3f})".format(ep, time.time() - begin, loss_tr_, rec_loss_tr_))            
+                print("epoch {:d} took {:.1f} training loss {:.3f} (rec:{:.3f})".format(ep, time.time() - begin, loss_tr_, topic_loss_tr_)) 
+                if ep % 100 == 0:
+                    W, = sess.run([tf.squeeze(words)], feed_dict=tr_dict)
+                    print("Top words for each topic:")
+                    for i in xrange(W.shape[1]):
+                        print("Topic %d: %s" % (i, ', '.join(top_words(W, i))))
     return losses, {"sess":sess, "total_loss": total_loss, "rec_loss": rec_loss, "rec_loss_val":rec_loss_val, 
                     "mat_values_tr": mat_values_tr, "mask_indices_tr": mask_indices_tr,
                     "mat_values_val":mat_values_val, "mask_indices_val":mask_indices_val,
@@ -154,6 +159,15 @@ def read_data():
     with open('./data/nyt/nyt_vocab.dat') as f:
         vocabs = f.readlines()
     vocabs = [x.strip().strip('\n').strip("'") for x in vocabs]
+
+    # use vocabs to define top words function
+    def top_words(words_embedding, topic_id, num_words=10):
+        '''
+        Return the top words associated with a particular embedding
+        '''
+        idx = np.argsort(-words_embedding[:, topic_id])
+        return [vocabs[i] for i in idx[0:num_words]]
+
     data['mat_shape'] = (len(vocabs), len(documents), 1)
 
     mask = []
@@ -168,13 +182,13 @@ def read_data():
     
     data['mask_indices_tr'] = np.array(mask)
     data['mat_values_tr'] = np.array(values)
-    return data
+    return data, top_words
 
 if __name__ == "__main__":
-    minibatch_size = 10000
+    minibatch_size = 1000000
     skip_connections = True
     units = 64
-    latent_features = 5
+    latent_features = 25
     learning_rate = 0.005
 
     opts ={'epochs': 5000,#never-mind this. We have to implement look-ahead to report the best result.
@@ -186,9 +200,9 @@ if __name__ == "__main__":
            'save':False,
            'output_file':'output',
            'architecture':[
-               {'type':'matrix_sparse', 'units':units, 'activation':tf.nn.relu},
+               {'type':'matrix_sparse', 'units':units},
                #{'type':'matrix_dropout_sparse'},
-               {'type':'matrix_sparse', 'units':units, 'activation':tf.nn.relu},
+               {'type':'matrix_sparse', 'units':units},
                #{'type':'matrix_dropout_sparse'},
                {'type':'matrix_sparse', 'units':latent_features, 'activation':None},#units before matrix-pool is the number of latent features for each movie and each user in the factorization
                {'type':'matrix_pool_sparse'},
@@ -197,11 +211,13 @@ if __name__ == "__main__":
                 'matrix_sparse':{
                     # 'activation':tf.nn.tanh,
                     # 'activation':tf.nn.sigmoid,
-                    'activation':tf.nn.relu,
+                    'activation':lambda x: tf.nn.sigmoid(x) * x,
+                    #'activation':tf.nn.relu,
                     # 'drop_mask':False,#whether to go over the whole matrix, or emulate the sparse matrix in layers beyond the input. If the mask is droped the whole matrix is used.
                     'pool_mode':'max',#mean vs max in the exchangeable layer. Currently, when the mask is present, only mean is supported
-                    'kernel_initializer': tf.random_normal_initializer(0, .01),
-                    'regularizer': tf.contrib.keras.regularizers.l2(.00001),
+                    #'kernel_initializer': tf.random_normal_initializer(0, .01),
+                    'kernel_initializer': tf.contrib.layers.xavier_initializer(),
+                    'regularizer': tf.contrib.keras.regularizers.l2(.0000001),
                     'skip_connections':skip_connections,
                 },
                 'dense':{#not used
